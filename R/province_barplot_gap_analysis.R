4eb1cadc77caae4605327016c4e6f1b8d956f8a7
pudiomkar <op24aai@herts.ac.uk>
2025-12-09 16:15:59 +0000

    dplyr added and get province_stats

--- R/province_barplot_gap_analysis.R
+++ R/province_barplot_gap_analysis.R
@@ -3,3 +3,15 @@ df <- read.csv("data/Case.csv", stringsAsFactors = FALSE)
 df[df == "-"] <- NA
 df$confirmed <- as.numeric(df$confirmed)
 df$province <- as.factor(df$province)
+
+#dplyr for data manipulation
+library(dplyr)
+
+province_stats <- df %>%
+  group_by(province) %>%
+  summarise(
+    mean_confirmed = mean(confirmed, na.rm = TRUE),
+    count = n()
+  )
+
+print(province_stats)
