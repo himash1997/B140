2c9adc6f3e63e0545c5b59627fc3fb2d08c83868
mounikav3002 <mv24abd@herts.ac.uk>
2025-12-09 15:26:20 +0000

    Checking normality for each groups

--- R/difference_in_means_test.R
+++ R/difference_in_means_test.R
@@ -3,3 +3,10 @@ df <- read.csv("data/Case.csv", stringsAsFactors = FALSE)
 df[df == "-"] <- NA
 df$confirmed <- as.numeric(df$confirmed)
 df$group <- as.factor(df$group)
+
+#Check normality for each group (Shapiro–Wilk)
+shapiro_true <- shapiro.test(analysis_data$confirmed[analysis_data$group == "TRUE"])
+shapiro_false <- shapiro.test(analysis_data$confirmed[analysis_data$group == "FALSE"])
+
+shapiro_true
+shapiro_false
